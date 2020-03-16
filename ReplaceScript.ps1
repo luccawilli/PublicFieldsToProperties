3776a12383d23cd70244f77194cf1b84520d9abd
Lucca Willi <wlu@rootserviceag.onmicrosoft.com>
2020-03-16 10:19:22 +0100

    Auto Checkout mit tfsvs hinzugefügt, geht nur mit nur einem Repo

--- ReplaceScript.ps1
+++ ReplaceScript.ps1
@@ -1,10 +1,12 @@
-$path = "C:\ProgrameP\Main\ReleaseCandidate\RC\Perigon\PerigonService\PerigonMobile\DTO"
-$pattern = "(public) ([A-Za-z0-9<>\?\[\]]*)(( )*) ([A-Za-z0-9]*)((;)|( =))"
+$path = "C:\ProgrameP\Main\ReleaseCandidate\RC\Perigon\Perigon\Service"
+$tfExePath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\tf.exe"
+$pattern = "(public) ([A-Za-z0-9<>\?\[\]]*)(( )*) ([A-Za-z0-9_-]*)((;)|( = ))"
 $files = Get-Childitem -Path $path -Filter "*.cs" -Recurse
 foreach ($f in $files){
     $content = Get-Content $f.FullName
     $match = $content -match $pattern
     if($match){
+	    & $tfExePath checkout $f.FullName
         foreach ($m in $match){
             $new = $m -replace "=","{ get; set; } ="   
             if($new -notlike "*=*"){
