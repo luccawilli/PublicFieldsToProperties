defbfa731e5d1cf4fef0b19a3f543281c08adcac
Lucca Willi <wlu@root.ch>
2024-08-15 15:51:45 +0200

    Just git, no tfvc

--- ReplaceScript.ps1
+++ ReplaceScript.ps1
@@ -1,17 +1,20 @@
-$path = "C:\ProgrameP\Main\ReleaseCandidate\RC\Perigon\Perigon\Service"
-$tfExePath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\tf.exe"
-$pattern = "(public) ([A-Za-z0-9<>\?\[\]]*)(( )*) ([A-Za-z0-9_-]*)((;)|( = ))"
-$files = Get-Childitem -Path $path -Filter "*.cs" -Recurse
+$path = "C:\root-git\perigon\PerigonDesktop"
+$pattern = "(public)(( static)|()) ([A-Za-z0-9._<>\?\[\]]*)(( )*) ([A-Za-z0-9_-]*)((;)|( = ))"
+$files = Get-Childitem -Path $path -Filter "*.cs" -Recurse -Exclude "*.resx.cs"
 foreach ($f in $files){
     $content = Get-Content $f.FullName
     $match = $content -match $pattern
     if($match){
-	    & $tfExePath checkout $f.FullName
         foreach ($m in $match){
-            $new = $m -replace "=","{ get; set; } ="   
-            if($new -notlike "*=*"){
-                $new = $new -replace ";"," { get; set; }"  
-            }
+			if($new -notlike "* _*"){
+				$new = $m -replace "=","{ get; set; } ="   
+				if($new -notlike "*=*"){
+					$new = $new -replace ";"," { get; set; }"  
+				}
+			}
+			else {
+				$new = $m -replace "public","private"
+			}
             $content = $content -replace [regex]::escape($m),$new
         }
         Set-Content -Path $f.FullName -Value $content
